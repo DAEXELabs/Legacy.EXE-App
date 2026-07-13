f30fd36aa3eb7fd9d56a35e8602da63b9e154a4a
RaemonBiz <raymondw138@gmail.com>
2026-07-13 18:42:29 -0500

    Fix commit

--- fix-html.mjs
+++ fix-html.mjs
@@ -18,6 +18,7 @@ const fixed = `<!doctype html>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Legacy.EXE</title>
+    <link rel="icon" type="image/png" href="/icons/icon-192.png" />
     <link rel="stylesheet" crossorigin href="/assets/${css}" />
     <link rel="manifest" href="/manifest.webmanifest" />
   </head>
